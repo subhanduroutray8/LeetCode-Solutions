c5fbef5bb773c79502604e96ecef891c1f83e6ee
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-07-05 16:05:42 +0530

    [1] [Time Beats: 33.68%] [Memory Beats: 57.82%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/1. Two Sum/1. Two Sum.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4            for(int j=i+1;j<nums.length;j++){
+5                if(nums[i]+nums[j] == target){
+6                    return new int[]{i,j};
+7                }
+8            }
+9        }
+10        return new int[]{};
+11    }
+12}
