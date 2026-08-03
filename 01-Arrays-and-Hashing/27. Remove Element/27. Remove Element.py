3e8dad600b5cbcbcda0ec5604dbe9bd46addf00d
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-03 18:14:34 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 16.67%] - Leet2Hub

--- 01-Arrays-and-Hashing/27. Remove Element/27. Remove Element.py
+++ 01-Arrays-and-Hashing/27. Remove Element/27. Remove Element.py
@@ -1,6 +1,11 @@
 1class Solution(object):
 2    def removeElement(self, nums, val):
-3        while val in nums:
-4            nums.remove(val)
-5        return len(nums)
-6        
+3        
+4        :type nums: List[int]
+5        :type val: int
+6        :rtype: int
+7        
+8        while val in nums:
+9            nums.remove(val)
+10        return len(nums)
+11        
