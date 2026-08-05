79d5cdd780fca202014b9519755d31e209a677ab
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-05 23:36:47 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 43.14%] - Leet2Hub

--- 01-Arrays-and-Hashing/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
+++ 01-Arrays-and-Hashing/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
@@ -1,10 +1,13 @@
 1class Solution(object):
 2    def removeDuplicates(self, nums):
 3        
-4        :type nums: List[int]
-5        :rtype: int
-6        
-7        for i in nums:
-8            while nums.count(i) > 1:
-9                nums.remove(i) 
-10        return len(nums)    
+4        i = 0
+5        if len(nums) == 1:
+6            return 1
+7
+8        for j in range(1,len(nums)):
+9            if nums[i] != nums[j]:
+10                i += 1
+11                nums[i] = nums[j]
+12        
+13        return i+1
