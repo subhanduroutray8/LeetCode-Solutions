0ad333afc5ec60464910a4ffa58e9d2b3385a1a7
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-05 13:22:42 +0530

    [217] [Time Beats: 50.21%] [Memory Beats: 13.54%] - Leet2Hub

--- 01-Arrays-and-Hashing/217. Contains Duplicate/217. Contains Duplicate.py
+++ 01-Arrays-and-Hashing/217. Contains Duplicate/217. Contains Duplicate.py
@@ -1,6 +1,11 @@
 1class Solution(object):
 2    def containsDuplicate(self, nums):
-3        if len(nums) == len(set(nums)):
-4            return False
-5        else:
-6            return True
+3        
+4        dct = {}
+5
+6        for i in range(len(nums)):
+7            if nums[i] in dct:
+8                return True
+9            dct[nums[i]] = 1
+10        
+11        return False
