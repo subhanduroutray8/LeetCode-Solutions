9cb86f6dfea0314ae0743e299561c036ff2e9de8
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-05 10:03:16 +0530

    [0000] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/0000. Set Mismatch/0000. Set Mismatch.py
@@ -0,0 +1,1 @@
+class Solution(object):    def findMaxConsecutiveOnes(self, nums):        count = 0        max_count = 0        for i in range(0,len(nums)):            if nums[i] == 1:                count += 1            else:                count = 0            if count > max_count:                max_count = count        return max_count 
