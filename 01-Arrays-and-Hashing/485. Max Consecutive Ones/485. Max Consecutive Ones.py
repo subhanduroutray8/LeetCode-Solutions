ef0fedcc61fc073e3dff57b88118a099459014bf
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-08 23:20:11 +0530

    [485] [Time Beats: 90.32%] [Memory Beats: 54.32%] - Leet2Hub

--- 01-Arrays-and-Hashing/485. Max Consecutive Ones/485. Max Consecutive Ones.py
+++ 01-Arrays-and-Hashing/485. Max Consecutive Ones/485. Max Consecutive Ones.py
@@ -1,17 +1,14 @@
 1class Solution(object):
 2    def findMaxConsecutiveOnes(self, nums):
-3
-4        count = 0
-5        max_count = 0
-6        for i in range(0,len(nums)):
-7            if nums[i] == 1:
+3        
+4        max = 0
+5        count = 0
+6        for i in nums:
+7            if i == 1:
 8                count += 1
 9            else:
 10                count = 0
-11
-12            if count > max_count:
-13                max_count = count
-14
-15        return max_count 
-16
-17
+11            if count > max:
+12                max = count 
+13
+14        return max
