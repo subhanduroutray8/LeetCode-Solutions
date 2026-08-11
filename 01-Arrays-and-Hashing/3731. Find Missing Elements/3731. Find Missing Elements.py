68b367ce4e5bd46bad9c0b952b317369b39990f8
Subhandu Sekhar Routray <subhanduroutray8@gmail.com>
2026-08-11 19:21:33 +0530

    [3731] [Time Beats: 67.25%] [Memory Beats: 57.89%] - Leet2Hub

--- 01-Arrays-and-Hashing/3731. Find Missing Elements/3731. Find Missing Elements.py
+++ 01-Arrays-and-Hashing/3731. Find Missing Elements/3731. Find Missing Elements.py
@@ -1,17 +1,15 @@
 1class Solution(object):
 2    def findMissingElements(self, nums):
-3        
-4        :type nums: List[int]
-5        :rtype: List[int]
-6        
-7        lst = []
-8        s = min(nums)
-9        l = max(nums)
+3
+4        
+5        lst = []
+6        s = min(nums)
+7        l = max(nums)
+8
+9        st = set(nums)
 10
-11        st = set(nums)
-12
-13        for i in range(s,l):
-14            if i not in st:
-15                lst.append(i)
-16        
-17        return lst
+11        for i in range(s,l):
+12            if i not in st:
+13                lst.append(i)
+14        
+15        return lst
